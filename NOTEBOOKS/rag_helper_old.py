1b6f0ec716b974cd1a778c6d2dcd0d6419c3991f
Denis K <denis911@gmail.com>
2026-07-21 20:15:34 +0200

    starting new notebook for HW 5

--- NOTEBOOKS/rag_helper_old.py
+++ NOTEBOOKS/rag_helper_old.py
@@ -23,23 +23,34 @@ class RAGBase:
         llm_client,
         instructions=INSTRUCTIONS,
         prompt_template=PROMPT_TEMPLATE,
+        course='llm-zoomcamp',
         model='gpt-5.4-mini'
     ):
         self.index = index
         self.llm_client = llm_client
         self.instructions = instructions
+        self.course = course
         self.prompt_template = prompt_template
         self.model = model
 
     def search(self, query, num_results=5):
-        return self.index.search(query, num_results=num_results)
+        boost_dict = {'question': 3.0, 'section': 0.5}
+        filter_dict = {'course': self.course}
+
+        return self.index.search(
+            query,
+            num_results=num_results,
+            boost_dict=boost_dict,
+            filter_dict=filter_dict
+        )
 
     def build_context(self, search_results):
         lines = []
 
         for doc in search_results:
-            lines.append(doc['filename'])
-            lines.append(doc['content'])
+            lines.append(doc['section'])
+            lines.append('Q: ' + doc['question'])
+            lines.append('A: ' + doc['answer'])
             lines.append('')
 
         return '\n'.join(lines).strip()
@@ -61,10 +72,10 @@ class RAGBase:
             input=input_messages
         )
 
-        return response
+        return response.output_text
 
     def rag(self, query):
         search_results = self.search(query)
         prompt = self.build_prompt(query, search_results)
-        response = self.llm(prompt)
-        return response.output_text
+        answer = self.llm(prompt)
+        return answer
